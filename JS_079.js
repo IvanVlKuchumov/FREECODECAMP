96e3b3e00f4c2ff650693cccbc0410c7abdb248e
IvanVlKuchumov <kuchumoviv@gmail.com>
2022-12-04 22:52:00 +0300

    Create JS_079.js
    Profile Lookup
    We have an array of objects representing different people in our contacts lists.
    
    A lookUpProfile function that takes name and a property (prop) as arguments has been pre-written for you.
    
    The function should check if name is an actual contact's firstName and the given property (prop) is a property of that contact.
    
    If both are true, then return the "value" of that property.
    
    If name does not correspond to any contacts then return the string No such contact.
    
    If prop does not correspond to any valid properties of a contact found to match name then return the string No such property.

---
+++ JS_079.js
@@ -0,0 +1,46 @@
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+  for (let i = 0; i < contacts.length; i++) {
+    if (name == contacts[i]["firstName"]) {
+      for (let j = 0; j < contacts[i].length; j++){
+        if (prop === contacts[i][j]) {
+        return contacts[i][prop];
+        }
+      }
+    return "No such property"  
+    }
+        return "No such contact"
+      
+
+  // Only change code above this line
+}
+
+lookUpProfile("Akira", "likes");
